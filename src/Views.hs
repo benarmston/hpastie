1c8ca380fc9b1c50caccddb20b0e32f7f48e843f
Ben Armston <ben.armston@googlemail.com>
2011-02-22 00:45:06 +0000

    Tidy view code

--- src/Views.hs
+++ src/Views.hs
@@ -30,42 +30,42 @@ pasteList pastes = layout "All pastes" $ do
 
 pasteForm :: [String] -> Paste -> Template
 pasteForm errors paste = layout "Paste form" $ do
-    H.ul ! A.class_ "errors" $ do
-        forM_ errors (H.li . H.toHtml)
-    H.form ! A.method "POST" $ do
+    ul ! class_ "errors" $ do
+        forM_ errors (li . toHtml)
+    H.form ! method "POST" $ do
         H.label "Title"
-        H.input ! A.name "title" ! A.size "50" ! A.value (H.stringValue $ pasteTitle paste)
-        H.br
+        input ! name "title" ! size "50" ! value (toValue $ pasteTitle paste)
+        br
         H.label "Syntax"
         -- H.select ! A.name "syntax" ! (A.value $ H.stringValue $ pasteSyntax paste)
         --    forM_ [""] (\o -> H.option ! value (stringValue o) $ string o)
-        H.br
-        H.textarea ! A.name "contents" ! A.rows "20" ! A.cols "76" $ H.string $ pasteContents paste
-        H.br
-        H.input ! A.type_ "submit" ! A.value "Save"
+        br
+        textarea ! name "contents" ! rows "20" ! cols "76" $ toHtml $ pasteContents paste
+        br
+        input ! type_ "submit" ! value "Save"
 
 
 pasteToHtml ::  Paste -> Template
 pasteToHtml paste = layout "Paste" $ do
-    H.div ! (A.id uid) $ do
-        H.h2 $ pTitle paste
-        H.p ! A.class_ "timestamp" $ formattedTime
-        H.pre $ formattedCode
+    div ! (id uid) $ do
+        h2 $ pTitle paste
+        p ! class_ "timestamp" $ formattedTime
+        pre $ formattedCode
     where contents = filter (/='\r') $ pasteContents paste
           --syntax = pasteSyntax paste
-          formattedCode = H.string $ contents
+          formattedCode = toHtml $ contents
           timestamp = pasteTimestamp paste
-          formattedTime = H.string $ formatTime defaultTimeLocale "%F %R UTC" timestamp
-          uid = H.stringValue . show $ pasteId paste
+          formattedTime = toHtml $ formatTime defaultTimeLocale "%F %R UTC" timestamp
+          uid = toValue $ pId paste
 
 
 layout :: String -> Html -> Template
-layout page_title page_body start_time current_time = H.docTypeHtml $ do
-    H.head $ do
-        H.title $ H.string page_title
-    H.body $ do
+layout page_title page_body start_time current_time = docTypeHtml $ do
+    head $ do
+        H.title $ toHtml page_title
+    body $ do
         page_body
-        H.div ! A.id "footer" $ do
+        div ! id "footer" $ do
             string "Config generated at " >> toHtml start_time
             string ". Page generated at " >> toHtml current_time
 
@@ -75,7 +75,11 @@ pTitle = toHtml . pasteTitle
 
 
 pUrl :: Paste -> AttributeValue
-pUrl paste = stringValue $ "/paste/" ++ (show $ pasteId paste)
+pUrl paste = toValue $ "/paste/" ++ pId paste
+
+
+pId :: Paste -> String
+pId = show . pasteId
 
 
 instance ToHtml UTCTime where
